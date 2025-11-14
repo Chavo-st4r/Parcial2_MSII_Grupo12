e62cc26db4d8b902db3df0803dbcd987eef142fd
Ch4vo-st4r <braian.rmrz@gmail.com>
2025-11-14 15:23:34 -0300

    Agrego funcion dividir ramirez Arreglado

--- tests/test_dividirramirez.py
+++ tests/test_dividirramirez.py
@@ -1,4 +1,5 @@
-from funciones.dividir import dividir 
-def test_dividir(): 
-assert dividir(10, 2) == 5 
-assert dividir(5, 0) is None 
+from funciones.dividir_ramirez import dividir
+
+def test_dividir():
+    assert dividir(10, 2) == 5
+    assert dividir(5, 0) is None
