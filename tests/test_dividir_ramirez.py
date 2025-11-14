a18a1b54bcac96899f8b08bb09c5bbd7da52aaaa
Chavito <braian.rmrz@gmail.com>
2025-11-14 15:53:46 -0300

    Rename

--- tests/test_dividir_ramirez.py
+++ tests/test_dividir_ramirez.py
@@ -1,6 +1,6 @@
 #Arreglo
-from funciones.dividir_ramirez import dividir_ramirez
+from funciones.dividir_ramirez import dividir
 
 def test_dividir_ramirez():
-    assert dividir_ramirez(10, 2) == 5
-    assert dividir_ramirez(5, 0) is None
+    assert dividir(10, 2) == 5
+    assert dividir(5, 0) is None
